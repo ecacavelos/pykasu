c3ec8ee78104edf952a7170ff02c8c18e3fe7d93
edimary89 <edimary89@gmail.com>
2014-01-31 14:36:27 +0000

    se agregan archivos faltantes en el commit anterior

---
+++ pykasu-web/WebRoot/plugins/plugin_112v2.js
@@ -0,0 +1,259 @@
+var salaryAmount;
+
+var pm_fiscalPeriodMounth = null;
+function onloadForm() {
+	loadScript('js/pykFiscalComun.js');
+
+	var valueOld;
+	var salaryAmount = '';
+	var dateField = document.getElementById('paymentDate');
+
+	if (document.getElementById('formStatus').value == 'insert_mode') {
+		document.getElementById('paymentDate').value = document
+				.getElementById('suggestedPaymentDate').value;
+	}
+	v_onload = 1;
+	orirecti();
+	v_onload = 0;
+
+	// El form 112 es anual. Y el mes (fijo) de vencimiento es 5.
+	document.getElementById('fiscalPeriodMounth').value = 5;
+	
+//	document.getElementById('c55').disabled = 'true';
+//	document.getElementById('c57').disabled = 'true';
+//	document.getElementById('c59').disabled = 'true';
+//	document.getElementById('c61').disabled = 'true';
+//	document.getElementById('c63').disabled = 'true';
+//	document.getElementById('c65').disabled = 'true';
+//	document.getElementById('c67').disabled = 'true';
+//	document.getElementById('c69').disabled = 'true';
+//	document.getElementById('c71').disabled = 'true';
+}
+
+function calculateBeforePlugin() {
+
+}
+
+function calculateAfterPlugin() {
+
+	orirecti();
+	pm_ruc = document.getElementById('ruc').value.toUpperCase();
+	pm_paymentDate = document.getElementById('paymentDate').value;
+	pm_fiscalPeriodMounth = document.getElementById('fiscalPeriodMounth').value;
+	pm_fiscalPeriodYear = removeCommas(document.getElementById('fiscalPeriodYear').value);
+	pm_called = 1;
+	if (PORC_MORA == 0 || PORC_INTERES == 0 || CONTRAVENSION_CMOV == 0	|| CONTRAVENSION_SMOV == 0) {
+		pm_called = 0;
+	}
+
+	calcPorcentajeMoras();
+	/*
+	 * var tmp39 = getValueFormatless('c39'); var tmp35 =
+	 * getValueFormatless('c35'); var tmp54 = getValueFormatless('c54');
+	 */
+
+	/*
+	 * if (tmp39 > 0) { //
+	 * document.getElementById('c35').value=getValueFormatless('c83');
+	 * document.getElementById('c35').value =
+	 * document.getElementById('c83').value; } else {
+	 * document.getElementById('c35').value = ''; if (tmp39 == 0) { //
+	 * document.getElementById('c54').value=getValueFormatless('c83');
+	 * document.getElementById('c54').value = document
+	 * .getElementById('c83').value; } }
+	 * 
+	 * if ((salaryAmount == null) || (salaryAmount == 0)) { retriveSalary(); }
+	 * 
+	 * if (document.getElementById('c76').value > salaryAmount) {
+	 * document.getElementById('c90').value =
+	 * document.getElementById('c76').value; } else {
+	 * document.getElementById('c90').value = ''; }
+	 */
+
+	// reCalc();
+}
+
+/*
+ * Si la declaracion es RECTIFICATIVA no se calcula contravenci?n
+ */
+function calcPorcentajeMoras() {
+
+	if (pm_called == 0) {
+		getFiscalMoraInfo();
+		return;
+	}
+
+	var norectificativa = 1;
+	if (document.getElementById('declarationType').value == '| 2 | RECTIFICATIVA') {
+		norectificativa = 0;
+	}
+
+	if (CONTRAVENSION_CMOV && CONTRAVENSION_CMOV != null) {
+		//multa
+		document.getElementById('c22').value = CONTRAVENSION_CMOV
+				* norectificativa;
+		if (document.getElementById('c22').value != null)
+			colocarpuntos(document.getElementById('c22'));
+	}
+
+	var tmp1 = removeCommas(document.getElementById('c26').value);
+	var tmp2 = removeCommas(document.getElementById('c22').value);
+	var op = tmp1 - tmp2;
+	if (op < 0) {
+		op = 0;
+	}
+
+	if (PORC_MORA && PORC_MORA != null) {
+		document.getElementById('c27').value = (PORC_MORA * op).toFixed(0);
+		if (document.getElementById('c27').value < 0) {
+			document.getElementById('c27').value = 0;
+		}
+		if (document.getElementById('c27').value != null)
+			colocarpuntos(document.getElementById('c27'));
+	}
+	if (PORC_INTERES && PORC_INTERES != null) {
+		document.getElementById('c28').value = (PORC_INTERES * op).toFixed(0);
+		if (document.getElementById('c28').value < 0) {
+			document.getElementById('c28').value = 0;
+		}
+		if (document.getElementById('c28').value != null)
+			colocarpuntos(document.getElementById('c28'));
+	}
+
+	refreshVars();
+	TRGc24();
+	TRGc26a();
+	TRGc26b();
+	TRGc29();
+
+	var tmp = (0).toFixed(0);
+
+}
+
+// Funcion que se realizo para esta regla //# SI (C[N]=0, 0, C[N+1]). 
+function checkIfContent(cellToTest, targetCellId){
+	var cellCondition = document.getElementById(targetCellId);
+	// Si el valor de la celda es nulo o cero, desabilitamos la celda dependiente.
+	if(cellToTest.value == null || cellToTest.value == 0){
+	        cellCondition.value=0;
+	        cellCondition.disabled = 'true';
+	}
+	// Si existe un valor, habilitamos la celda dependiente.
+	else {
+//	        cellCondition.value="";
+	        cellCondition.disabled = '';
+	        cellCondition.focus();
+	}
+	refreshVars();
+	TRGc72();
+	TRGc73();
+	
+}
+
+function getFormType() {
+	return '112v2';
+}
+
+function orirecti() {
+
+	try {
+		var decType = document.getElementById('declarationType');
+		// var rectPPN = document.getElementById('rectificativePPN');
+
+		rectPPN.disabled = 'true';
+
+		/*
+		 * if (decType.value == "| 2 | RECTIFICATIVA") { rectPPN.disabled = ''; }
+		 * else { rectPPN.value = ''; rectPPN.disabled = 'true'; }
+		 */
+
+		pm_called = 1; // Para que no llame al servidor.
+
+		calcPorcentajeMoras();
+
+	} catch (e) {
+		// alert('error al calcular Origen');
+	}
+
+}
+
+/*
+ * Para ddjj ORIGINALES y RECTIFICATIVAS: a?o tiene que ser menor al actual Para
+ * ddjj CLAUSURA: a?o igual
+ */
+function controlaAnho() {
+	var fiscalYear = getValueFormatless('fiscalPeriodYear');
+	var declaType = document.getElementById('declarationType').value;
+	var fecha = new Date();
+	var anhoActual = getActualYear();
+
+	if (fiscalYear != 0) {
+		// Si es clausura tiene que ser el a?o actual
+		if (declaType == '| 5 | CLAUSURA') {
+			if (fiscalYear != anhoActual) {
+				alert('Para las ddjj de tipo CLAUSURA, el anho debe ser el año actual');
+				document.getElementById('fiscalPeriodYear').value = "";
+				document.getElementById('fiscalPeriodYear').focus();
+				return;
+			}
+		} else {// Si no es clausura, el a?o tiene que ser menor al actual
+			if (fiscalYear >= anhoActual) {
+				alert('Para las ddjj ORIGINALES y RECTIFICATIVAS, el año debe ser menor al año actual');
+				document.getElementById('fiscalPeriodYear').value = "";
+				document.getElementById('fiscalPeriodYear').focus();
+				return;
+			}
+		}
+	}
+	getPorcentajeMoras(document.getElementById('fiscalPeriodYear'));
+}
+
+/*
+ * Retorna el mes valido de presentacion para el formulario (que es anual) Para
+ * el 112 el mes es Mayo(5)
+ */
+function getValidMonth() {
+	return 5;
+}
+
+/**
+ * function reCalc(){ refreshVars();
+ * 
+ * TRGc39(); TRGc39_0(); TRGc35(); TRGc35_0(); TRGc35_1(); TRGc37(); TRGc54();
+ * TRGc54_0(); TRGc62(); TRGc64(); TRGc72(); TRGc72_0(); TRGc74(); TRGc74_0();
+ * TRGc76(); TRGc78(); TRGc80(); TRGc80_0(); }
+ */
+
+function beforeSave() {
+	/*REsolucion general nro 107 --> NO se pueden presentar declaraciones sin movimiento*/
+	var isEmpty = true;
+	for (i = 10 ; i <= 170 ; i++) // Recorrer todas las celdas
+	{
+		if(document.getElementById("c"+i) != null) 
+			if (document.getElementById("c"+i).value != ''){ // hay ALGO en el campo
+				if (document.getElementById("c"+i).value != '0' ){ // Hay un monto diferente a 0 
+					isEmpty = false; // existe un mvto.
+					break;
+				}
+			}		
+	}
+	if (isEmpty){//Sin movimientos
+		alert ('Sr. usuario, les informamos que por disposición de la resolución general nro. 107 del Ministerio de Hacienda, no se pueden presentar declaraciones juradas SIN MOVIMIENTO por este medio.');
+		return false; 
+	}
+	if (document.getElementById("estado-ruc").value == "SUSPENSION TEMPORAL"){
+		alert ('Sr. usuario, les informamos que por disposición de la resolución general nro. 107 del Ministerio de Hacienda, los contribuyentes cuyos RUCs se encuentren en estado de Suspensión Temporal, no podrán presentar DDJJ por este medio.');
+		return false; 
+	}
+	
+	/*REsolucion general nro 107 --> NO se pueden presentar declaraciones sin movimiento*/
+	return true;
+}
+
+function notNullVaule(value) {
+	if (value > 0) {
+		return value;
+	} else {
+		return 0;
+	}
+}
---
+++ pykasu-web/WebRoot/plugins/plugin_120v3.js
@@ -0,0 +1,187 @@
+
+function onloadForm(){
+	loadScript('js/pykFiscalComun.js');
+	var valueOld;
+	if(document.getElementById('formStatus').value == 'insert_mode'){
+		document.getElementById('paymentDate').value = document.getElementById('suggestedPaymentDate').value;
+	}
+	v_onload = 1;
+	orirecti();
+	v_onload = 0; 
+
+}
+
+function calculateBeforePlugin(){
+
+}
+
+function calculateAfterPlugin(){
+	orirecti();
+	pm_ruc = document.getElementById('ruc').value.toUpperCase();
+	pm_paymentDate = document.getElementById('paymentDate').value;
+	pm_fiscalPeriodMounth = document.getElementById('fiscalPeriodMounth').value;
+	pm_fiscalPeriodYear = removeCommas(document.getElementById('fiscalPeriodYear').value);	
+	pm_called = 1;
+	//calcPorcentajeMoras();
+}
+
+
+function calcPorcentajeMoras(){
+	//document.getElementById('c56').value = "50000";
+	if(CONTRAVENSION_CMOV && CONTRAVENSION_CMOV != null){
+		document.getElementById('c56').value =  CONTRAVENSION_CMOV;//*norectificativa;
+		if(document.getElementById('c56').value != null)
+  		   colocarpuntos(document.getElementById('c56'));
+	}
+//	if (pm_called == 0){
+//		getFiscalMoraInfo();
+//		return;
+//	}
+//	//alert("calcPorcentajeMoras() CONTRAVENSION_CMOV:" + CONTRAVENSION_CMOV + "  PORC_MORA:" + PORC_MORA + "  PORC_INTERES: "+ PORC_INTERES);
+//	
+//	var norectificativa = 1;
+//	if (document.getElementById('declarationType').value=='| 2 | RECTIFICATIVA'){
+//		norectificativa = 0;
+//	}
+//	
+	
+	
+//	if(CONTRAVENSION_CMOV && CONTRAVENSION_CMOV != null){
+//		document.getElementById('c113').value =  CONTRAVENSION_CMOV*norectificativa;
+//		if(document.getElementById('c113').value != null)
+//  		   colocarpuntos(document.getElementById('c113'));
+//	}
+//	
+//	var tmp115 = removeCommas(document.getElementById('c115').value);
+//	var tmp113 = removeCommas(document.getElementById('c113').value);
+//	var op = tmp115 - tmp113
+//	if(op < 0){
+//		op = 0
+//	}
+//	
+//	if (PORC_MORA && PORC_MORA != null){
+//		document.getElementById('c56').value =  (PORC_MORA * op).toFixed(0);//*norectificativa;
+//		if(document.getElementById('c56').value != null)
+//  		   colocarpuntos(document.getElementById('c56'));
+//	}		
+//	if (PORC_INTERES && PORC_INTERES != null){
+//		document.getElementById('c117').value =  (PORC_INTERES * op).toFixed(0);//*norectificativa;
+//		if(document.getElementById('c117').value != null)
+//  		   colocarpuntos(document.getElementById('c117'));
+//	}
+
+	//alert("calcPorcentajeMoras() c113:" + document.getElementById('c113').value + "  c116:" + document.getElementById('c116').value + "  c117: "+ document.getElementById('c117').value);	
+//	refreshVars();
+//	TRG_C114();
+//	TRG_C111_1();
+//	TRG_C111_2();
+//	TRG_C115_1();
+//	TRG_C115_2();
+//	TRG_C116();
+//	TRG_C117();
+//	TRG_C118();
+//	checkC57();
+
+	//alert("calcPorcentajeMoras() c113:" + document.getElementById('c113').value + "  c116:" + document.getElementById('c116').value + "  c117: "+ document.getElementById('c117').value);	
+
+//	var tmp = (0).toFixed(0);
+}
+
+
+function getFormType(){
+	return '120v3';
+}
+
+function orirecti(){
+
+	try{
+		var decType = document.getElementById('declarationType');
+//		var rectPPN = document.getElementById('rectificativePPN');
+		
+//		rectPPN.disabled = 'true';
+//		
+//		if (decType.value == "| 2 | RECTIFICATIVA"){			
+//			rectPPN.disabled = '';
+//		}
+//		else{			
+//			rectPPN.value = '';
+//			rectPPN.disabled = 'true';
+//		}
+	
+		pm_called = 1;//para que no llame al servidor..
+		
+		calcPorcentajeMoras();	
+	
+	}catch(e){
+		//alert('error al calcular Origen');
+	}	
+
+}
+
+//function checkC57(){
+//	var tmp_c56= parseInt(removeCommas(document.getElementById("c56").value));
+//	var tmp_c57= parseInt(removeCommas(document.getElementById("c57").value));
+//	var tmp_c58= parseInt(removeCommas(document.getElementById("c58").value));
+//	
+//	if(tmp_c57 > tmp_c56){
+//		alert('El valor del campo 57 no puede ser mayor al campo 56');
+//		document.getElementById("c57").value='';
+//		return -1;
+//	}else{
+//		if(tmp_c58+tmp_c57 > tmp_c56){
+//			alert('La suma de los campos 57 y 58 no puede ser mayor al campo 56');
+//			document.getElementById("c57").value='';
+//			document.getElementById("c58").value='';
+//			return -1;
+//		}
+//	
+//	}
+//	
+//}
+
+function giveFormType(){
+ return 'mensual';
+}
+
+function periodControl(){
+	if (!controlFiscalPeriod(giveFormType())){
+		return false;
+	}
+	return true;
+}
+
+function beforeSave(){
+	if (checkC57()<0){
+		return false;
+	}
+	
+	if (!periodControl()){
+		return false;
+		}
+	
+	/*REsolucion general nro 107 --> NO se pueden presentar declaraciones sin movimiento*/
+	var isEmpty = true;
+	for (i = 10 ; i <= 66 ; i++) // Recorrer todas las celdas
+	{
+		if(document.getElementById("c"+i) != null) 
+			if (document.getElementById("c"+i).value != ''){ // hay ALGO en el campo
+				if (document.getElementById("c"+i).value != '0' ){ // Hay un monto diferente a 0 
+					isEmpty = false; // existe un mvto.
+					break;
+				}
+			}		
+	}
+	if (isEmpty){//Sin movimientos
+		alert ('Sr. usuario, les informamos que por disposici\u00f3n de la resoluci\u00f3n general nro. 107 del Ministerio de Hacienda, no se pueden presentar declaraciones juradas SIN MOVIMIENTO por este medio.');
+		return false; 
+	}
+	if (document.getElementById("estado-ruc").value == "SUSPENSION TEMPORAL"){
+		alert ('Sr. usuario, les informamos que por disposici\u00f3n de la resoluci\u00f3n general nro. 107 del Ministerio de Hacienda, los contribuyentes cuyos RUCs se encuentren en estado de Suspensi\u00f3n Temporal, no podr\u00e1n presentar DDJJ por este medio.');
+		return false; 
+	}
+	
+	/*REsolucion general nro 107 --> NO se pueden presentar declaraciones sin movimiento*/
+	return true;
+
+}
+
